3de583361c7010ea87c174e1fb906b96ede69961
jo <jo.rigg8@gmail.com>
2022-12-10 06:18:13 +0000

    day 3 problem 2

--- AoC2022/DayThree.cs
+++ AoC2022/DayThree.cs
@@ -14,12 +14,8 @@
                 var second = line.Substring(line.Length / 2, line.Length / 2);
                 var intersect = first.Intersect(second);
 
-                // what priority does this interestcing item have (it will always only be one)
-                // a-z = 1 - 26
-                // A-Z = 27 - 52
-                var c = intersect.First();
-                // a = unicode 97 so 97 - 96 = 1 A = unicode 65 so 65 - 38 = 26
-                total += char.IsUpper(c) ? (c - 38) : (c - 96);
+                // what priority does this interestcing item have (it will always only be one)              
+                total += GetItemPriority(intersect.First());
          
             }
             Console.WriteLine(total);
@@ -27,7 +23,21 @@
 
         public void RunProblemTwo()
         {
+            var total = 0;
+            // split into groups of 3 and which letter is common to all 3
+            foreach (var line in input.Chunk(3))
+            {
+                total += GetItemPriority(line[0].Intersect(line[1]).Intersect(line[2]).First());
+            }
+            Console.WriteLine(total);
+        }
 
+        private int GetItemPriority(char item)
+        {
+            // a-z = 1 - 26
+            // A-Z = 27 - 52
+            // a = unicode 97 so 97 - 96 = 1 A = unicode 65 so 65 - 38 = 26
+            return char.IsUpper(item) ? (item - 38) : (item - 96);
         }
 
     }
